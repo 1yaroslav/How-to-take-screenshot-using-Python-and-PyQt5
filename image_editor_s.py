31c36a43d42054b2e66e3d634c31bb3753db09ee
1yaroslav <77401951+1yaroslav@users.noreply.github.com>
2021-07-12 11:11:31 +0300

    Add files via upload

---
+++ image_editor_s.py
@@ -0,0 +1,79 @@
+import sys
+import time
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+from PIL import ImageGrab
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Take screenshot'
+        self.left = 250
+        self.top = 250
+        self.width = 200
+        self.height = 90
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        """
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280, 40)
+        """
+
+        self.button = QPushButton('&Сделать скриншот', self)
+        self.button.move(20, 20)
+
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.button.clicked.connect(self.on_click)
+        self.show()
+    @pyqtSlot()
+    def on_click(self):
+        import os
+        path = "screenshots"
+        for el in path:
+            if os.path.exists(el):
+                time.sleep(2)
+                img = ImageGrab.grab()
+                time_now = time.strftime("%Y%m%d%h%M%S")
+                name = f"screen{time_now}"
+                img.save(f"{path}/{name}.png", "png")
+                name_img = name
+                QMessageBox.question(self, 'Скриншот сделан!', f"Скриншот сохранен в директории {path}/" + name_img,QMessageBox.Ok,QMessageBox.Ok)
+                break
+            else:
+                try:
+                    os.mkdir(path)
+                except OSError:
+                    #print("Директория %s уже есть на этом компьютере" % path)
+                    time.sleep(2)
+                    img = ImageGrab.grab()
+                    time_now = time.strftime("%Y%m%d%h%M%S")
+                    name = f"screen{time_now}"
+                    img.save(f"{path}/{name}.png", "png")
+                    name_img = name
+                    QMessageBox.question(self, 'Скриншот сделан!', f"Скриншот сохранен в директории {path}/" + name_img,QMessageBox.Ok,QMessageBox.Ok)
+                    break
+                else:
+                    time.sleep(2)
+                    img = ImageGrab.grab()
+                    time_now = time.strftime("%Y%m%d%h%M%S")
+                    name = f"screen{time_now}"
+                    img.save(f"{path}/{name}.png", "png")
+                    name_img = name
+                    QMessageBox.question(self, 'Скриншот сделан!', f"Скриншот сохранен в директории {path}/" + name_img,QMessageBox.Ok,QMessageBox.Ok)
+                    break
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
